67e47e0522e75d7d8ba9c09eb79290bac9c9cf81
charlie yin <charlie.t.yin@gmail.com>
2022-07-24 16:33:22 -0500

    arrow upgrade nav

--- Assets/Scripts/Upgrade Menu/Upgrades.cs
+++ Assets/Scripts/Upgrade Menu/Upgrades.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
 
@@ -9,6 +10,7 @@ public class Upgrades : MonoBehaviour
     //public Button firstChoice;
     //public Button secondChoice;
     //public Button thirdChoice;
+    public GameObject upgradeFirstButton;
 
     public TextMeshProUGUI firstChoiceText;
     public TextMeshProUGUI secondChoiceText;
