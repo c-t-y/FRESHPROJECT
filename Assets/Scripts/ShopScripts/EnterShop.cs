d7d729fc63c12a9ef0806e27c0568783e4f066b1
slippindip <octopii@gmail.com>
2022-07-25 18:15:46 -0400

    stage and spawn updates

--- Assets/Scripts/ShopScripts/EnterShop.cs
+++ Assets/Scripts/ShopScripts/EnterShop.cs
@@ -11,7 +11,7 @@ public class EnterShop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
