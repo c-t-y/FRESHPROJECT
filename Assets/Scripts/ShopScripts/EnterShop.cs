4a97096091cb886a983929d5997e90a628fd8b8f
charlie yin <charlie.t.yin@gmail.com>
2022-07-25 14:28:45 -0500

    shop

---
+++ Assets/Scripts/ShopScripts/EnterShop.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterShop : MonoBehaviour
+{
+    public GameObject player;
+    public Animator transition;
+    public GameObject crossfader;
+    public GameObject playerSpawnMarker;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Debug.Log("test");
+            StartCoroutine(GoToShop());
+        }
+    }
+
+    IEnumerator GoToShop()
+    {
+        crossfader.SetActive(true);
+        Debug.Log("waitttt");
+
+        yield return new WaitForSeconds(0.5f);
+        player.transform.position = playerSpawnMarker.transform.position;
+
+
+        Debug.Log("woooo");
+        crossfader.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/ShopScripts/ExitShop.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitShop : MonoBehaviour
+{
+    public GameObject player;
+    public Animator transition;
+    public GameObject crossfader;
+    public GameObject shopSpawnMarker;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Debug.Log("test");
+            StartCoroutine(LeaveShop());
+        }
+    }
+
+    IEnumerator LeaveShop()
+    {
+        crossfader.SetActive(true);
+        Debug.Log("waitttt");
+
+        yield return new WaitForSeconds(0.5f);
+        player.transform.position = new Vector3(shopSpawnMarker.transform.position.x, shopSpawnMarker.transform.position.y - 1, shopSpawnMarker.transform.position.z);
+
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("wow");
+        crossfader.SetActive(false);
+    }
+
+}
