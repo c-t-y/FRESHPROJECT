dc50352f9a5740629ff4b43d7ed27e274cb5f1d3
slippindip <octopii@gmail.com>
2022-07-25 22:47:42 -0400

    changed cages and factory

--- Assets/Scripts/ShopScripts/ExitShop.cs
+++ Assets/Scripts/ShopScripts/ExitShop.cs
@@ -7,9 +7,19 @@ public class ExitShop : MonoBehaviour
     public GameObject player;
     public Animator transition;
     public GameObject crossfader;
-    public GameObject shopSpawnMarker;
+    public GameObject shopEntranceMarker;
 
+    void Start()
+    {
+        Invoke("ShopEntrance", 3f);
+    }
 
+    public void ShopEntrance()
+    {
+       
+        shopEntranceMarker = GameObject.FindGameObjectWithTag("ShopEntranceMarker");
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == player)
@@ -26,7 +36,7 @@ public class ExitShop : MonoBehaviour
         Debug.Log("waitttt");
 
         yield return new WaitForSeconds(0.5f);
-        player.transform.position = new Vector3(shopSpawnMarker.transform.position.x, shopSpawnMarker.transform.position.y - 1, shopSpawnMarker.transform.position.z);
+        player.transform.position = new Vector3(shopEntranceMarker.transform.position.x, shopEntranceMarker.transform.position.y - 1, shopEntranceMarker.transform.position.z);
 
         yield return new WaitForSeconds(0.5f);
         Debug.Log("wow");
